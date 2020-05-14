04794de2ae13dab31a7eee378b0c31fff992b20b
charlesmartinreed <charlesmartinreed@icloud.com>
2020-05-14 17:48:49 -0500

    Add countdown ticker using hooks.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  let [futureDate, setFutureDate] = useState(
+    new Date("Thu May 24 2020 16:45:28 GMT-0500").getTime()
+  );
+
+  let [currentDate, setCurrentDate] = useState(new Date().getTime());
+
+  let [distance, setDistance] = useState(futureDate - currentDate);
+
+  let [distanceObj, setDistanceObj] = useState({
+    days: Math.floor(distance / day),
+    hours: Math.floor((distance % day) / hour),
+    minutes: Math.floor((distance % hour) / minute),
+    seconds: Math.floor((distance % minute) / second),
+  });
+
+  function useInterval(cb, delay) {
+    const savedCallback = useRef();
+
+    useEffect(() => {
+      savedCallback.current = cb;
+    }, [cb]);
+
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+
+  useInterval(() => {
+    // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+
+    setCurrentDate(new Date().getTime());
+    setDistance(futureDate - currentDate);
+
+    let days = Math.floor(distance / day);
+    let hours = Math.floor((distance % day) / hour);
+    let minutes = Math.floor((distance % hour) / minute);
+    let seconds = Math.floor((distance % minute) / second);
+
+    setDistanceObj({ days, hours, minutes, seconds });
+
+    // update the DOM element representing the particular time interval here
+    document.getElementById("days").innerText = distanceObj.days;
+    document.getElementById("hours").innerText = distanceObj.hours;
+    document.getElementById("minutes").innerText = distanceObj.minutes;
+    document.getElementById("seconds").innerText = distanceObj.seconds;
+  }, second);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,16 +70,16 @@ function App() {
       <div id="countdownTicker">
         <ul>
           <li>
-            <span>00</span>Days
+            <span id="days"></span>Days
           </li>
           <li>
-            <span>00</span>Hours
+            <span id="hours"></span>Hours
           </li>
           <li>
-            <span>00</span>Minutes
+            <span id="minutes"></span>Minutes
           </li>
           <li>
-            <span>00</span>Seconds
+            <span id="seconds"></span>Seconds
           </li>
         </ul>
       </div>
