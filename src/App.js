77d83a5463af92c0400a081b4e1e5d849992b28d
charlesmartinreed <charlesmartinreed@icloud.com>
2020-05-14 16:22:12 -0500

    Add countdown element to app.js.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 id="head">How Soon Is Then?</h1>
       </header>
+
+      <div id="countdownTicker">
+        <ul>
+          <li>
+            <span>00</span>Days
+          </li>
+          <li>
+            <span>00</span>Hours
+          </li>
+          <li>
+            <span>00</span>Minutes
+          </li>
+          <li>
+            <span>00</span>Seconds
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
