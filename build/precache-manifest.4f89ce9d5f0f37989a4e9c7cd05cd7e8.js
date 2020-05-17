3191d5329d6cca2b538d4c15486780aa1c969fe8
charlesmartinreed <charlesmartinreed@icloud.com>
2020-05-16 19:42:27 -0500

    Add build folder.

---
+++ build/precache-manifest.4f89ce9d5f0f37989a4e9c7cd05cd7e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f734330dca163ce3f6649994cfa67a68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d94ffb315a4f850406c",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "5c2ce67019130bc2e918",
+    "url": "/static/css/main.3633d7a8.chunk.css"
+  },
+  {
+    "revision": "4d94ffb315a4f850406c",
+    "url": "/static/js/2.4b6792f1.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.4b6792f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2ce67019130bc2e918",
+    "url": "/static/js/main.5f106aff.chunk.js"
+  },
+  {
+    "revision": "03b543f7a44ae1d65900",
+    "url": "/static/js/runtime-main.80a8feeb.js"
+  }
+]);
